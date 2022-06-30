d9353deb3d3c81c787641817ece3e0c38c58f963
Sebastián Guardo <33914923+Sebasgl23@users.noreply.github.com>
2022-06-30 17:22:31 -0500

    Firts API Tests  (#3)
    * Setup Mocha Configuration
    
    * Fixing EOF errors
    
    * MyFirstApiConsume Tests
    
    * Fixing extra lines after EOL
    
    * Making changes due to the observations
    
    * Fixing Conflicts
    
    * Removing lines in package.json
    
    * Fixing package errors
    
    * Putted all the tests into the describe clause

---
+++ test/MyFirstApiConsume.test.js
@@ -0,0 +1,79 @@
+const axios = require('axios');
+const { expect } = require('chai');
+const { StatusCodes } = require('http-status-codes');
+
+describe('First Api Tests', () => {
+
+
+it('Consume GET Service', async () => {
+    const response = await axios.get('https://httpbin.org/ip');
+  
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.have.property('origin');
+  });
+
+  it('Consume GET Service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
+  
+    const response = await axios.get('https://httpbin.org/get', { query });
+  
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.query).to.eql(query);
+  });
+
+  
+  it('Consume PATCH Service' , async () => {
+    const body = {
+      name: 'Sebastian',
+      city: 'Cartagena'
+    };
+
+    const response = await axios.patch('https://httpbin.org/patch' ,  body );
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.args).to.equal(body.data);
+
+  });
+
+  it('Consume HEAD Service' , async () => {
+
+    const response = await axios.head('https://httpbin.org/get');
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.body).to.equal(undefined);
+
+  })
+
+  it('Consume PUT Service' , async () => {
+    const body = {
+      name: 'Carlos',
+      age: '45',
+      city: 'Orlando'
+
+    };
+
+    const response = await axios.put('https://httpbin.org/put' ,  body );
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.args).to.equal(body.data);
+
+  });
+
+
+  it('Consume DELETE Service' , async () => {
+    const body = {
+      name: 'Carlos',
+      age: '45'
+    };
+
+    const response = await axios.delete('https://httpbin.org/delete' ,  body );
+
+    expect(response.status).to.equal(StatusCodes.OK);
+
+  });
+
+});
