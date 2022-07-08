41e6dd0325bc0e70cd44eb5551049fc25a083823
Sebastián Guardo <33914923+Sebasgl23@users.noreply.github.com>
2022-07-08 14:45:39 -0500

    Consume GET Method Fix

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -13,21 +13,39 @@ const githubUserName = 'aperdomob';
 const repoName = 'jasmine-json-report';
 
 describe('Consume GET Services from GITHUB API', () => {
+  let userResponse;
+
+  before(async () => {
+    userResponse = await axios.get(`${urlBase}/users/${githubUserName}`, {
+      headers: {
+        Authorization: `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+  });
   it('Get User Data', async () => {
-    const response = await axios.get(`${urlBase}/users/${githubUserName}`);
-    expect(response.status).to.equal(StatusCodes.OK);
-    expect(response.data).to.have.property('name');
-    expect(response.data).to.have.property('company');
-    expect(response.data).to.have.property('location');
-    expect(response.data.name).to.equal('Alejandro Perdomo');
-    expect(response.data.company).to.equal('Perficient Latam');
-    expect(response.data.location).to.equal('Colombia');
+    expect(userResponse.status).to.equal(StatusCodes.OK);
+    expect(userResponse.data).to.have.property('name');
+    expect(userResponse.data).to.have.property('company');
+    expect(userResponse.data).to.have.property('location');
+    expect(userResponse.data.name).to.equal('Alejandro Perdomo');
+    expect(userResponse.data.company).to.equal('Perficient Latam');
+    expect(userResponse.data.location).to.equal('Colombia');
+  });
+
+  let reposResponse;
+  let repo;
+
+  before(async () => {
+    reposResponse = await axios.get(`${userResponse.data.repos_url}`, {
+      headers: {
+        Authorization: `token ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    repo = reposResponse.data.find((element) => element.name === repoName);
   });
 
   it('Get Repos List and validate data', async () => {
-    const response = await axios.get(`${urlBase}/users/${githubUserName}/repos`);
-    expect(response.status).to.equal(StatusCodes.OK);
-    const repo = response.data.find((element) => element.name === repoName);
+    expect(reposResponse.status).to.equal(StatusCodes.OK);
     expect(repo.name).to.equal('jasmine-json-report');
     expect(repo).to.have.property('private');
     expect(repo.private).to.equal(false);
@@ -36,18 +54,31 @@ describe('Consume GET Services from GITHUB API', () => {
   });
 
   it('Download Repo in a Zip File', async () => {
-    const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repoName}/zipball/master`);
+    const response = await axios.get(`${repo.url}/zipball/master`);
     expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.headers['content-type']).to.equal('application/zip');
+  });
+
+  let contentsRespone;
+  let file;
+
+  before(async () => {
+    contentsRespone = await axios.get(`${repo.url}/contents`);
+
+    file = contentsRespone.data.find((element) => element.name === 'README.md');
   });
 
   it('Get list of files in the Repo', async () => {
-    const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repoName}/contents`);
-    expect(response.status).to.equal(StatusCodes.OK);
-    const file = response.data.find((element) => element.name === 'README.md');
-    expect(file.name).to.equal('README.md');
-    expect(file.path).to.equal('README.md');
-    expect(file).to.have.property('sha');
-    const download = await axios.get(`https://raw.githubusercontent.com/${githubUserName}/${repoName}/master/README.md`);
+    expect(contentsRespone.status).to.equal(StatusCodes.OK);
+    expect(file).to.containSubset({
+      path: 'README.md',
+      name: 'README.md',
+      sha: '360eee6c223cee31e2a59632a2bb9e710a52cdc0'
+    });
+  });
+
+  it('Download the file and check md5', async () => {
+    const download = await axios.get(`${file.download_url}`);
     expect(download.status).to.equal(StatusCodes.OK);
     expect(md5(download.data)).to.equal('497eb689648cbbda472b16baaee45731');
   });
